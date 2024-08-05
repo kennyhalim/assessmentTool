1fc3f922faa3744a50b0fdb349db7a45e8b7dddf
kennyhalim <kennyhalim98@gmail.com>
2024-08-05 13:06:38 -0700

    update results

--- results.js
+++ results.js
@@ -10,29 +10,19 @@ document.addEventListener('DOMContentLoaded', function() {
         title = "Maintain Alertness & Safety";
         severityClass = 'alert-safe';
         countermeasures = [
-            "Increase hydration",
-            "Avoid sugary drinks & snacks",
-            "Eat high protein, low carbs (e.g., energy bar/nuts/yogurt/peanut butter = LOW GLYCEMIC FOODS)",
-            "Try stimulating aromas (e.g., spearmint gum/peppermint tea/citrus fruits/eucalyptus oil)"
+            "Select the appropriate operational countermeasures strategies to ensure you get through the shift alert and safely."
         ];
     } else if (checkedQuestions.includes(4) && !checkedQuestions.includes(5) && !checkedQuestions.includes(6)) {
-        title = "Mild to Moderate";
+        title = "Ensure you have a safe commute home.";
         severityClass = 'alert-mild';
         countermeasures = [
-            "Strategic use of caffeine to increase alertness",
-            "Increase interaction with co-workers",
-            "Increase physical activity (walk/gym/stationary bike)",
-            "Use of light exposure/therapy to reduce short term circadian misalignment (where available)"
+            "To avoid sleep debt the next day, ensure you get a minimum of 7-9 hours of sleep."
         ];
     } else if (checkedQuestions.includes(5) && !checkedQuestions.includes(6)) {
-        title = "Moderate to Severe";
+        title = "Consult with a co-worker/supervisor";
         severityClass = 'alert-moderate';
         countermeasures = [
-            "Increase check-in frequency when working alone",
-            "Utilize safety/process checklists to minimize errors",
-            "Defer to a second opinion / ask coworker to double check your work",
-            "Consider alternatives to driving home",
-            "Delay decision-making where appropriate"
+            "Before continuing with work to see what can be done to reduce your risk and improve health and safety."
         ];
     } else if (checkedQuestions.includes(6)) {
         title = "Immediately inform your supervisor to determine appropriate strategies.";
